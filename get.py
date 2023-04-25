98d021d4a97cab7cde38c6769e4393d14208da6d
brmcdoug <brmcdoug@cisco.com>
2023-04-25 14:08:33 -0700

    timeout

--- get.py
+++ get.py
@@ -1,11 +1,14 @@
 from pygnmi.client import gNMIclient
 import json
 
+host = ("10.152.254.106",57400)
+
 if __name__ == '__main__':
 
-	path = ['openconfig-interfaces:interfaces/interface[name=Loopback0]','Cisco-IOS-XR-ipv4-bgp-oper:bgp/bpm-instances-table/bpm-instances']
+	path = ['openconfig-interfaces:interfaces/interface[name=Loopback0]']#,'Cisco-IOS-XR-ipv4-bgp-oper:bgp/bpm-instances-table/bpm-instances']
 	
-	with gNMIclient(target=("10.152.254.106",57400),username="cisco",password="cisco123",insecure=True) as gc:	
+
+	with gNMIclient(target=host,username="cisco",password="cisco123",insecure=True, timeout=30) as gc:	
 		get_result = gc.get(path=path,encoding='json_ietf',datatype='all',) 
 
 	print(json.dumps(get_result, indent=4))
