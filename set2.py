b8868b5f92a415fe3d0ecb045fa27106c483a7e1
brmcdoug <brmcdoug@cisco.com>
2023-04-25 15:38:59 -0700

    ip

--- set2.py
+++ set2.py
@@ -15,9 +15,55 @@ if __name__ == '__main__':
 							"description":"testing pygnmi to create an interface"
 						}
 
-				)
+				),
+				("openconfig-interfaces:interfaces/interface[name=Loopback3]/subinterfaces",
+                    {
+                            "subinterface": [
+                                {
+                                    "index": 0,
+                                    "openconfig-if-ip:ipv4": {
+                                        "addresses": {
+                                            "address": [
+                                                {
+                                                    "ip": "10.0.0.233",
+                                                    "config": {
+                                                        "ip": "10.0.0.233",
+                                                        "prefix-length": 32
+                                                    },
+                                                    "state": {
+                                                        "ip": "10.0.0.233",
+                                                        "prefix-length": 32,
+                                                        "origin": "STATIC"
+                                                    }
+                                                }
+                                            ]
+                                        }
+                                    },
+                                    "openconfig-if-ip:ipv6": {
+                                        "addresses": {
+                                            "address": [
+                                                {
+                                                    "ip": "fc00:0:233::1",
+                                                    "config": {
+                                                        "ip": "fc00:0:233::1",
+                                                        "prefix-length": 128
+                                                    },
+                                                    "state": {
+                                                        "ip": "fc00:0:233::1",
+                                                        "prefix-length": 128,
+                                                        "origin": "STATIC",
+                                                        "status": "PREFERRED"
+                                                    }
+                                                }
+                                            ]
+                                        }
+                                    }
+                                }
+                            ]
+                    }
+            )
+	    ]
 
-			] 
 	with gNMIclient(target=("10.251.254.106",57400),username="cisco",password="cisco123",insecure=True) as gc:
 		update_result = gc.set(update=conf,encoding='json_ietf')
 	
